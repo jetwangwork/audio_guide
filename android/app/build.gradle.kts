97b8a322f5789ee4adb2d79fd7e27c0ac4169267
王靖捷的G16\Wang <wangufw@gmail.com>
2026-04-24 10:38:07 +0800

    調整ndk版本

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.audio_guide"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion =  "27.0.12077973" // flutter.ndkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
